99f5faeb80c767c54a3e788539ccc3a3dfe90bd5
Ryan Brownewell <ryan.brownewell@gmail.com>
2021-10-01 22:13:38 -0400

    Added enum for suit and value

--- App.tsx
+++ App.tsx
@@ -2,10 +2,33 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 // import Card from "./components/Card";
 
+enum Suit {
+  clubs = "C",
+  diamonds = "D",
+  hearts = "H",
+  spades = "S",
+}
+
+enum CardValue {
+  ace = "A",
+  two = "2",
+  three = "3",
+  four = "4",
+  five = "5",
+  six = "6",
+  seven = "7",
+  eight = "8",
+  nine = "9",
+  ten = "10",
+  jack = "J",
+  queen = "Q",
+  king = "K",
+}
+
 // Move to own file?
 type CardProps = {
-  value: string;
-  suit: string;
+  value: CardValue;
+  suit: Suit;
   // Key right now should be "value+suit", and has to be manually set as such.
   // See if there's a way to set that dynamically. It is required to be unique
   // from the other keys: https://sentry.io/answers/unique-key-prop/
@@ -20,11 +43,15 @@ const Card = (props: CardProps) => {
   );
 };
 
-let kingCard: CardProps = { value: "King", suit: "Clubs", key: "KingClubs" };
+let kingCard: CardProps = {
+  value: CardValue.king,
+  suit: Suit.clubs,
+  key: `${CardValue.king + Suit.clubs}`,
+};
 let queenCard: CardProps = {
-  value: "Queen",
-  suit: "Hearts",
-  key: "QueenHearts",
+  value: CardValue.queen,
+  suit: Suit.hearts,
+  key: `${CardValue.queen + Suit.hearts}`,
 };
 
 // Map the cards to components
